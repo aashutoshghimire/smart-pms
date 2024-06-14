538ac6bb52c22653c3f690656c69de02d6908d0d
Ashutosh Ghimire <aashutoshghimire7@gmail.com>
2024-06-14 17:38:43 -0400

    updated message in about page

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -4,13 +4,12 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'Notices';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
+    <p>Woking on this ^^</p>
 
-    <code><?= __FILE__ ?></code>
-</div>
+   </div>
