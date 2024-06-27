1e39f5b470700d0becf6609307597aa42d2d6c8d
Ashutosh Ghimire <aashutoshghimire7@gmail.com>
2024-06-27 09:19:07 -0400

    Developed by changed

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -75,7 +75,7 @@ AppAsset::register($this);
 <footer class="footer mt-auto py-3 text-muted">
     <div class="container">
         <p class="float-start">&copy; <?= 'SMART Lab TMS' ?> <?= date('Y') ?></p>
-        <p class="float-end"><?= 'Developed by Ashutosh Ghimire' ?></p>
+        <p class="float-end"><?= 'Developed by SMART Lab, WSU, Dayton' ?></p>
     </div>
 </footer>
 
